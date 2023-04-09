efe7f4276c31665c1b0559c1ba42969085fa53bc
Simas <simasvenskus@gmail.com>
2023-04-09 15:31:53 +0300

    Models for inventory Ingredients, MenuItem, RecipeRequirement, Purchase

--- food_tracker/inventory/models.py
+++ food_tracker/inventory/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+# Represents individual ingrediant's
+class Ingredient(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    quantity = models.FloatField(max_length=10, default=0)
+    unit = models.CharField(max_length=10)
+    price_per_unit = models.FloatField(max_length=5, default=0)
+
+    def __str__(self):
+        return f"{self.name}: {self.quantity}"
+
+
+# Represents restaurant's menu
+class MenuItem(models.Model):
+    title = models.CharField(max_length=40, unique=True)
+    price = models.FloatField(max_length=5)
+
+    def __str__(self):
+        return f"{self.title}, price: {self.price}"
+
+
+# Represents individual menu item recipie
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0)
+
+    def __str__(self):
+        return f"{self.menu_item} recipe"
+
+
+# Represents purchases and time of the purchase of the menu
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.menu_item} purchased at: {self.timestamp}"
